65b98035c64779597d4efb128864e7eedb3c477e
Timothy O'Connell <toconnell@provenir.com>
2017-09-11 16:42:18 -0400

    added header, title and done methods to TestBase.

--- src/main/scala/ua/test/base/TestBase.scala
+++ src/main/scala/ua/test/base/TestBase.scala
@@ -2,6 +2,8 @@ package ua.test.base
 
 object TestBase {
   
+  val BORDER_CHARACTER = "*"
+  
   def tprint (text: String): Unit = {
     println (text)
   }
@@ -13,6 +15,30 @@ object TestBase {
   def tprint (indentCount: Int, text1: String, text2: String): Unit = {
     tprint (indent (indentCount) + text1, text2)
   }
+  
+  def tprintDone (): Unit = {
+    
+    println
+    println ("Done.")
+    println
+  }
+  
+  def tprintHeader (headerText: String): Unit = {
+    
+		println
+		println ("************  " + headerText + "  ************")
+		println    
+  }
+  
+  def tprintTitle (title: String): Unit = {
+		
+		val length = title.length()
+		
+		println (BORDER_CHARACTER * (length + 12))
+		println (BORDER_CHARACTER + BORDER_CHARACTER + "    " + title + "    " + BORDER_CHARACTER + BORDER_CHARACTER)
+		println (BORDER_CHARACTER * (length + 12))
+		println
+	}
   
   def indent (count: Int): String = {
     
